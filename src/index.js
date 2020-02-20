fa99443b91554644ce65efd1316fca22465520be
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-02-20 20:52:49 +0100

    React toegepast

--- src/index.js
+++ src/index.js
@@ -1,62 +1,70 @@
-// import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
 import Store from './js/Store';
 import Post from './js/Post';
-import {autorun} from 'mobx';
+import {useObserver} from 'mobx-react-lite';
 
-const renderPosts = posts => {
-  const $postList = document.querySelector('.posts');
-  $postList.innerHTML = '';
-  posts.forEach(post => {
-    $postList.appendChild(createPost(post));
-  });
+const store = new Store();
+
+ store.seed([
+   new Post({
+     picture: 'vegan-salad-bowl.jpg',
+     title: 'Vegan salad bowl',
+     user: 'Sarah Van Den Berghe',
+     description: 'Een heerlijke vegan avondmaal gemaakt! Vol met vitaminen!  😋',
+     tags: ['Vegetarisch', 'Avondeten', 'Gezond']
+   }),
+ ]);
+
+ store.seed([
+   store.posts[0].addComment('Laura', 'Yummy!'),
+   store.posts[0].addComment('Anna', 'Ziet er goed uit!')
+ ]);
+
+const App = () => {
+  return useObserver(() => (
+    <>
+      <ul className="posts">
+        <li className="post">
+          <button className="post__like post__like--true">
+            <span className="hidden">Vind ik leuk</span>
+          </button>
+          <img
+            className="post__img"
+            src={store.posts[0].picture}
+            alt={store.posts[0].title}
+          />
+          <div className="post__info">
+            <header>
+              <h3 className="post__title">{store.posts[0].title}</h3>
+              <p className="post__user">{store.posts[0].user}</p>
+            </header>
+            <p className="post__description">{store.posts[0].description}</p>
+            <ul className="post__tags">
+              <li className="tag">Een tag</li>
+            </ul>
+          </div>
+          <ul className="post__comments">
+            <li className="comment">
+              <span className="comment__user">User: </span>Comment
+            </li>
+          </ul>
+          <form className="form">
+            <div className="wrapper">
+              <label className="hidden">Voeg een comment toe</label>
+              <input
+                className="comment__input"
+                type="text"
+                placeholder="Type een reactie"
+              />
+              <button className="comment__submit" type="submit" />
+            </div>
+          </form>
+        </li>
+      </ul>
+    </>
+  ));
 };
 
-const createPost = post => {
-  const $li = document.createElement('li');
-  $li.classList.add('post', post.liked ? 'post__like--true' : 'post__like--false');
-
-  $li.innerHTML = post.createHTML();
-  $li.querySelector('.post__like').addEventListener('click', () => toggleLike(post));
-  return $li;
-};
-
-const toggleLike = post => {
-  post.liked ? post.toggleLike(false) : post.toggleLike(true);
-};
-
-const init = () => {
-  const store = new Store();
-
-  autorun(() => {
-    renderPosts(store.posts);
-  });
-
-  // Seed posts
-  store.seed([
-    new Post({
-      picture: '../src/assets/img/vegan-salad-bowl.jpg',
-      title: 'Vegan salad bowl',
-      user: 'Sarah Van Den Berghe',
-      description: 'Een heerlijke vegan avondmaal gemaakt! Vol met vitaminen!  😋',
-      tags: ['Vegetarisch', 'Avondeten', 'Gezond']
-    }),
-
-    new Post({
-      picture: '../src/assets/img/sushi-rolls.jpg',
-      title: 'Sushi rolls',
-      user: 'Sarah Van Den Berghe',
-      description: 'Voor de eerste keer zelf sushi rolls proberen te maken!',
-      tags: ['Aziatisch', 'Gezond']
-    })
-  ]);
-
-  // Seed comments
-  store.seed([
-    store.posts[0].addComment('Laura', 'Yummy!'),
-    store.posts[0].addComment('Anna', 'Ziet er goed uit!')
-  ]);
-
-  window.store = store;
-};
-
-init();
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/js/Post.js
+++ src/js/Post.js
@@ -17,32 +17,6 @@ class Post {
   addComment(user, comment) {
     this.comments.push({user: user, comment: comment});
   }
-
-  // Niet finaal, al gemaakt voor volgende fases
-  createHTML() {
-    return `<button class="post__like"><span class="hidden">Vind ik leuk</span></span></button>
-    <img class="post__img" src="${this.picture}" alt="${this.title}">
-    <div class="post__info">
-      <header>
-        <h3 class="post__title">${this.title}</h3>
-        <p class="post__user">${this.user}</p>
-      </header>
-      <p class="post__description">${this.description}</p>
-      <ul class="post__tags">
-        ${this.tags.map(tag => { return `<li class="tag">${tag}</li>`;}).join('')}
-      </ul>
-    </div>
-    <ul class="post__comments">
-      ${this.comments.map(comment => { return `<li class="comment"><span class="comment__user">${comment.user}:</span> ${comment.comment}</li>`;}).join('')}
-    </ul>
-    <form class="form">
-      <div class="wrapper">
-        <label class="hidden">Voeg een comment toe</label>
-        <input class="comment__input" type="text" placeholder="Type een reactie">
-        <button class="comment__submit" type="submit">
-      </div>
-    </form>`;
-  }
 }
 
 decorate(Post, {
